578fe07bf5cc5503ac48eccf87498715e65d0d32
AsmusWE <146201076+AsmusWE@users.noreply.github.com>
2024-11-19 18:58:31 +0100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -6,9 +6,9 @@ df_spot = pd.read_csv('./data/Elspotprices.csv', sep=';', decimal=',')
 
 df_balancing = pd.read_csv('./data/RegulatingBalancePowerdata.csv', sep=';', decimal=',')
 df_aFRR = pd.read_csv('./data/AfrrReservesNordic.csv', sep=';', decimal=',')
-df_aFRR_activation = pd.read_excel('./data/AfrrActivatedAutomatic.xlsx')
-df_ninja_2022 = pd.read_csv('./data/ninja_wind_55.0000_13.0000_corrected_2022.csv', skiprows=3) # Skip first 3 rows because they are the header
-df_ninja_2023 = pd.read_csv('./data/ninja_wind_55.0000_13.0000_corrected_2023.csv', skiprows=3)
+#df_aFRR_activation = pd.read_excel('./data/AfrrActivatedAutomatic.xlsx')
+#df_ninja_2022 = pd.read_csv('./data/ninja_wind_55.0000_13.0000_corrected_2022.csv', skiprows=3) # Skip first 3 rows because they are the header
+#df_ninja_2023 = pd.read_csv('./data/ninja_wind_55.0000_13.0000_corrected_2023.csv', skiprows=3)
 df_wind = pd.read_csv('./data/ElectricityBalanceNonv.csv', sep=';', decimal=',')
 
 #p_RT = pd.concat([df_ninja_2022,df_ninja_2023], axis=0)['electricity']
@@ -19,8 +19,8 @@ lambda_DA = df_spot[df_spot['PriceArea']=='DK2']['SpotPriceDKK'] # Spot price: D
 lambda_B = df_balancing[df_balancing['PriceArea']=='DK2']['ImbalancePriceDKK'] # Balancing price: DKK/MWh (mFRR, used as aFRR)
 
 lambda_RES = df_aFRR[df_aFRR['PriceArea']=='DK2']['aFRR_DownCapPriceDKK'] # aFRR capacity price: DKK/MW
-gamma_RES = df_aFRR_activation[df_aFRR_activation['PriceArea']=='DK2']['aFRR_DownActivated'] # aFRR down-regulation activation: MW
-gamma_RES = (gamma_RES > 0).astype(int) # Makes the activation binary
+#gamma_RES = df_aFRR_activation[df_aFRR_activation['PriceArea']=='DK2']['aFRR_DownActivated'] # aFRR down-regulation activation: MW
+#gamma_RES = (gamma_RES > 0).astype(int) # Makes the activation binary
 # "aFRR Downward regulation purchased (MW)" -- not used because we assume that they always buy
 
 
