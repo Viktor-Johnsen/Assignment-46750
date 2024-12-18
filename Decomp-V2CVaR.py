27d8022fdc48aee6d3fd716c6c3c975633094fd6
AsmusWE <146201076+AsmusWE@users.noreply.github.com>
2024-12-18 13:43:03 +0100

    Implementerede decomponering med CVaR. Den bedste er allinsub, men den har fejl

---
+++ Decomp-V2CVaR.py
@@ -0,0 +1,230 @@
+import gurobipy as gp
+from gurobipy import GRB
+import pandas as pd
+import numpy as np
+from IPython.display import display
+
+from load_data import p_RT, lambda_DA, lambda_B, lambda_RES# , gamma_RES
+
+def solve_sub(p_DA, p_RT, p_RES, lambda_B, lambda_RES, lambda_DA,gamma_RES, pi, T, zeta, beta, alpha):
+        # One subproblem per scenario
+        M = 10000000000
+
+        submodel = gp.Model("sub")
+        submodel.Params.OutputFlag = 0 # Turn off output to console
+        #submodel.Params.DualReductions = 0 # Turn off dual reductions
+
+        Delta_down = submodel.addVars(T, lb=0, vtype=GRB.CONTINUOUS, name="Delta_down")
+        a_RES = submodel.addVars(T, lb=0, vtype=GRB.CONTINUOUS, name="a_RES")
+
+        # Variables to make extracting duals easier
+        p_DA_out = submodel.addVars(T, lb=0, vtype=GRB.CONTINUOUS, name="p_DA_out")
+        p_RES_out = submodel.addVars(T, lb=0, vtype=GRB.CONTINUOUS, name="p_RES_out")
+
+        # Slack variable
+        slack = submodel.addVar(lb=0, vtype=GRB.CONTINUOUS, name="slack")
+
+        # CVaR variable
+        eta = submodel.addVar(lb=0, vtype=GRB.CONTINUOUS, name="eta")
+        zeta_out = submodel.addVar(lb = -GRB.INFINITY,vtype=GRB.CONTINUOUS, name="zeta_out")
+
+        submodel.setObjective((1-beta)* gp.quicksum(
+                               (p_DA_out[t]*lambda_DA[t] +
+                                p_RES_out[t]*lambda_RES[t] - # New
+                                (Delta_down[t]+a_RES[t]*gamma_RES[t])*lambda_B[t]) # Changed
+                                for t in TT) - 
+                                M*slack +
+                                beta*(-1/(1-alpha)*eta) +
+                                beta*zeta_out, 
+                                GRB.MAXIMIZE)
+                                # NOTE: beta*zeta is missing, and must be added in the master problem
+
+        #1st stage constraints:
+        submodel.addConstrs((p_RT[t] + slack >= p_DA_out[t] - Delta_down[t] - a_RES[t]*gamma_RES[t] for t in TT), name="c_RT") # Changed
+        submodel.addConstrs((             p_DA_out[t] - Delta_down[t] - a_RES[t]*gamma_RES[t] + slack >= 0 for t in TT), name="c_NonnegativePhysicalDelivery") # Changed
+        submodel.addConstrs((a_RES[t] + slack >= p_RES_out[t] for t in TT), name="c_Activation") # New  
+
+        # CVaR constraints
+        submodel.addConstr(eta >= zeta_out -(gp.quicksum(
+                               (p_DA_out[t]*lambda_DA[t] +
+                                p_RES_out[t]*lambda_RES[t] - # New
+                                (Delta_down[t]+a_RES[t]*gamma_RES[t])*lambda_B[t]) # Changed
+                                for t in TT) - M*slack) 
+                                , name="c_CVaR")
+        
+        # Constraints to extract duals
+        submodel.addConstrs((p_DA_out[t] == p_DA[t] for t in TT), name="c_DA_out")
+        submodel.addConstrs((p_RES_out[t] == p_RES[t] for t in TT), name="c_RES_out")
+        submodel.addConstr(zeta_out == zeta, name="c_zeta_out")
+
+        submodel.optimize()
+
+        if submodel.status == GRB.OPTIMAL:
+                optimal_objective = submodel.objVal # Save optimal value of objective function
+                #print("Optimal objective:", optimal_objective)
+                #Delta_down_sol = np.array( [[Delta_down[w,t].x for t in TT] for w in WW] )
+                #a_RES_sol = np.array( [[a_RES[w,t].x for t in TT] for w in WW] )
+                p_DA_dual = [submodel.getConstrByName(f"c_DA_out[{t}]").Pi for t in range(T)]
+                p_RES_dual = [submodel.getConstrByName(f"c_RES_out[{t}]").Pi for t in range(T)]
+                zeta_dual = submodel.getConstrByName("c_zeta_out").Pi
+                Delta_down_sol = [Delta_down[t].x for t in range(T)]
+                a_RES_sol = [a_RES[t].x for t in range(T)]
+                p_DA_out_sol = [p_DA_out[t].x for t in range(T)]
+                p_RES_out_sol = [p_RES_out[t].x for t in range(T)]
+                #print(f"slack: {slack.x}, profit: {optimal_objective-beta*(-1/(1-alpha)*eta.x)}, objective: {optimal_objective}, eta: {eta.x}, zeta_dual: {zeta_dual}")
+                #print(f"p_DA_dual: {p_DA_dual}, p_RES_dual: {p_RES_dual}, zeta_dual: {zeta_dual}")
+                return optimal_objective, p_DA_dual, p_RES_dual, zeta_dual
+
+        else:
+                print("Optimization was not successful")
+                
+                print(submodel.status)
+                return None, None, None
+
+
+
+
+
+def solve_mas(W, T, pi, p_RT, lambda_B, lambda_DA, lambda_RES, gamma_RES, P_nom, beta, alpha):
+    VMAX = 100
+    WW = np.arange(W)
+    TT = np.arange(T)
+    UB = 1e6
+    LB = -1e6
+    v = 0
+    
+    # Initial var values
+    p_DA_sol = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5493019912819032, 0.0035644947925966863, 0.898930636153742, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    p_RES_sol = [1.0, 1.0, 0.9796299908538351, 0.6989696956205991, 1.0, 1.0, 1.0, 0.8540406193596299, 0.9929223720112418, 0.5083873415072301, 0.0, 0.898930636153742, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.7831102516010463, 1.0, 1.0, 1.0, 1.0]
+    zeta_sol = 2796.1729
+
+    p_DA_val = np.zeros((T,VMAX))
+    p_RES_val = np.zeros((T,VMAX))
+    zeta_val = np.zeros(VMAX)
+
+    model = gp.Model("MAS")
+    model.Params.OutputFlag = 0 # Turn off output to console
+
+    p_DA = model.addVars(T, lb=0, ub=P_nom , vtype=GRB.CONTINUOUS, name="p_DA")
+    p_RES = model.addVars(T, lb=0, ub=P_nom,vtype=GRB.CONTINUOUS, name="p_RES")
+    #gamma = model.addVar(lb=LB, ub=UB, vtype=GRB.CONTINUOUS, name="gamma")
+    gamma = model.addVars(WW, lb=LB, ub=UB, vtype=GRB.CONTINUOUS, name="gamma")
+    zeta = model.addVar(lb=-10000, ub=10000, vtype=GRB.CONTINUOUS, name="zeta")
+
+    #model.setObjective(gamma, GRB.MAXIMIZE) # Benders objective
+    model.setObjective(gp.quicksum(pi[w]*gamma[w] for w in WW) 
+                       + beta*zeta, GRB.MAXIMIZE) # Benders objective
+                       #NOTE: beta*zeta is added
+
+    LB_arr = np.zeros((W,VMAX))
+    lambDA_arr = np.zeros((W,T, VMAX))
+    lambRes_arr = np.zeros((W,T, VMAX))
+    lambzeta_arr = np.zeros((W,VMAX))
+
+    # Add Benders cuts
+    #while UB - LB > 0.1:
+    for v in range(VMAX):
+        if v > 0:
+                for w in WW:
+                        LB_temp, lambDA, lambRES, lambzeta = solve_sub(p_DA_val[:,v-1], p_RT[w,:], p_RES_val[:,v-1], lambda_B[w,:], lambda_RES[w,:], lambda_DA[w,:], gamma_RES[w,:], pi[w], T, zeta_val[v-1], beta, alpha)
+                        LB_arr[w,v] = LB_temp
+                        lambDA_arr[w,:,v] = lambDA
+                        lambRes_arr[w,:,v] = lambRES
+                        lambzeta_arr[w,v] = lambzeta
+                sub_obj = pi[0]*np.sum(LB_arr[:,v])
+                LB = max(LB, sub_obj+beta*zeta_val[v-1])
+
+                for w in WW:
+                        model.addConstr(gamma[w] <= LB_arr[w,v] + 
+                                        gp.quicksum(lambDA_arr[w,t,v]*(p_DA[t]-p_DA_val[t,v-1]) + 
+                                        lambRes_arr[w,t,v]*(p_RES[t]-p_RES_val[t,v-1] +
+                                        lambzeta_arr[w,v]*(zeta-zeta_val[v-1])) 
+                                        for t in TT))
+                #model.addConstr(gamma <= gp.quicksum(pi[w]*LB_arr[w,v] for w in WW) + 
+                #                gp.quicksum(lambDA_arr[w,t,v]*(p_DA[t]-p_DA_val[t,v-1]) + 
+                #                lambRes_arr[w,t,v]*(p_RES[t]-p_RES_val[t,v-1]) 
+                #                for w in WW for t in TT))
+        
+        model.optimize()
+        #model.addConstrs(p_DA[t] >=1 for t in TT)
+        if model.status == GRB.OPTIMAL:
+            # Update values
+            UB = model.objVal
+            p_DA_val[:,v] = [p_DA[t].x for t in TT]
+            #p_DA_val[:,v] = p_DA_sol
+            p_RES_val[:,v] = [p_RES[t].x for t in TT]
+            #p_RES_val[:,v] = p_RES_sol
+            zeta_val[v] = zeta.x
+            #zeta_val[v] = zeta_sol
+            #print(f"UB: {UB}, LB: {LB}")
+            #print(f"p_DA: {p_DA_val[:,v]}")
+            #print(f"p_RES: {p_RES_val[:,v]}")
+            print(zeta_val[v])
+            print(f"lambdazeta: {lambzeta_arr[:,v]}")
+            #print(f"lambdaDA: {lambDA_arr[:,:,v]}")
+            #print(f"lambdaRES: {lambRes_arr[:,:,v]}")
+
+        else:
+            model.write("model.lp")
+            print('Optimization of master was not successful')
+            print(model.status)
+            
+            break
+        v += 1
+        print(f"The upper bound is {UB}")
+        print(f"The lower bound is {LB}")
+        diff = UB - LB
+        print(f"The difference between UB and LB is {diff}")
+        if diff < 10:
+            break
+        
+
+    if model.status == GRB.OPTIMAL:
+         return model.objVal, p_DA_val[:,v], p_RES_val[:,v]
+
+
+
+
+show_plots = False # Usedd to toggle between plotting and not plotting...
+
+T=24 #hours that we offer in
+W=30 #scenarios/days, our training set
+beta = 0.1
+alpha = 0.9
+
+p_RT = p_RT.values[:W*T].reshape(W,T)
+lambda_B = lambda_B.values[:W*T].reshape(W,T)
+lambda_DA = lambda_DA.values[:W*T].reshape(W,T)
+lambda_RES = lambda_RES.values[:W*T].reshape(W,T)
+# gamma_RES = gamma_RES.values[:W*T].reshape(W,T)
+
+TT=np.arange(T)
+WW=np.arange(W)
+
+pi = np.ones(W)/W # Scenarios are assumed to be equiprobable
+P_nom = 1 # MW
+
+obj = np.zeros(W)
+p_DAs = np.zeros((W,T))
+Deltas = np.zeros((W,T))
+
+#lambda_B[lambda_B <= 0] = 0 # Just used to check smth
+
+gamma_RES = np.ones((W,T)) # Down-regulation activated in all hours
+gamma_RES[lambda_B >= lambda_DA]=0 # Down-regulation not activated in hours where balancing price is higher than DA price
+
+# Solve the MAS
+obj, p_DAs, p_Res = solve_mas(W, T, pi, p_RT, lambda_B, lambda_DA, lambda_RES, gamma_RES, P_nom, beta, alpha)
+
+
+p_DA_sol = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5493019912819032, 0.0035644947925966863, 0.898930636153742, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+
+p_RES_sol = [1.0, 1.0, 0.9796299908538351, 0.6989696956205991, 1.0, 1.0, 1.0, 0.8540406193596299, 0.9929223720112418, 0.5083873415072301, 0.0, 0.898930636153742, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.7831102516010463, 1.0, 1.0, 1.0, 1.0]
+
+zeta = 2796.1729
+sum = 0
+for w in WW:
+        obj, DA, RES, XE = solve_sub(p_DA_sol, p_RT[w,:], p_RES_sol, lambda_B[w,:], lambda_RES[w,:], lambda_DA[w,:], gamma_RES[w,:], pi[w], T, zeta, beta, alpha)
+        #print(obj)
+        sum += obj
+optimal_objective_comparison = sum/W + beta*zeta
---
+++ V25_CVaR_for_decomp.py
@@ -0,0 +1,174 @@
+import gurobipy as gp
+from gurobipy import GRB
+import pandas as pd
+import numpy as np
+from IPython.display import display
+
+from load_data import p_RT, lambda_DA, lambda_B, lambda_RES# , gamma_RES
+
+show_plots = False # Used to toggle between plotting and not plotting...
+
+T=24 #hours that we offer in
+W=30 #scenarios/days, our training set
+beta = 0.1
+alpha = 0.9
+
+p_RT = p_RT.values[:W*T].reshape(W,T)
+lambda_B = lambda_B.values[:W*T].reshape(W,T)
+lambda_DA = lambda_DA.values[:W*T].reshape(W,T)
+lambda_RES = lambda_RES.values[:W*T].reshape(W,T)
+
+TT=np.arange(T)
+WW=np.arange(W)
+
+pi = np.ones(W)/W # Scenarios are assumed to be equiprobable
+P_nom = 1 # MW
+
+obj = np.zeros(W)
+p_DAs = np.zeros((W,T))
+Deltas = np.zeros((W,T))
+
+gamma_RES = np.ones((W,T)) # Down-regulation activated in all hours
+gamma_RES[lambda_B >= lambda_DA]=0 # Down-regulation not activated in hours where balancing price is higher than DA price
+
+#2 Mathematical model
+model = gp.Model("V1")
+p_DA = model.addVars(T, lb=0, vtype=GRB.CONTINUOUS, name="p_DA")
+Delta_down = model.addMVar((W,T), lb=0, vtype=GRB.CONTINUOUS, name="Delta_down")
+# New variables
+p_RES = model.addVars(T, lb=0, vtype=GRB.CONTINUOUS, name="p_RES")
+a_RES = model.addMVar((W,T), lb=0, vtype=GRB.CONTINUOUS, name="a_RES")
+
+#CVaR variables
+zeta = model.addVar(lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS, name="zeta")
+eta = model.addVars(W, lb=0, vtype=GRB.CONTINUOUS, name="eta")
+
+model.setObjective((1-beta)* gp.quicksum(pi[w]*
+                        (p_DA[t]*lambda_DA[w,t] + 
+                        p_RES[t]*lambda_RES[w,t] - # New
+                        (Delta_down[w,t]+a_RES[w,t]*gamma_RES[w,t])*lambda_B[w,t]) # Changed
+                        for w in WW for t in TT) + 
+                        beta*(zeta - 1/(1-alpha)*gp.quicksum(pi[w]*eta[w] for w in WW)), 
+                        GRB.MAXIMIZE)
+
+#1st stage constraints:
+model.addConstrs((p_DA[t] <= P_nom for t in TT), name="c_Nom")
+model.addConstrs((p_RT[w,t] >= p_DA[t] - Delta_down[w,t] - a_RES[w,t]*gamma_RES[w,t] for w in WW for t in TT), name="c_RT") # Changed
+model.addConstrs((             p_DA[t] - Delta_down[w,t] - a_RES[w,t]*gamma_RES[w,t] >= 0 for w in WW for t in TT), name="c_NonnegativePhysicalDelivery") # Changed
+model.addConstrs((a_RES[w,t] >= p_RES[t] for w in WW for t in TT), name="c_Activation") # New  
+
+# CVaR constraints
+model.addConstrs((eta[w] >= zeta-
+                        gp.quicksum((p_DA[t]*lambda_DA[w,t] + 
+                        p_RES[t]*lambda_RES[w,t] - # New
+                        (Delta_down[w,t]+a_RES[w,t]*gamma_RES[w,t])*lambda_B[w,t]) # Changed
+                        for t in TT)  for w in WW), name="c_CVaR")
+
+model.optimize()
+
+if model.status == GRB.OPTIMAL:
+        print()
+        print(f'-------------------   RESULTS   -------------------')
+        optimal_objective = model.objVal # Save optimal value of objective function
+        #print("Optimal objective:", optimal_objective)
+        p_DA_sol = [p_DA[t].x for t in TT]
+        Delta_down_sol = np.array( [[Delta_down[w,t].x for t in TT] for w in WW] )
+        p_RES_sol = [p_RES[t].x for t in TT]
+        a_RES_sol = np.array( [[a_RES[w,t].x for t in TT] for w in WW] )
+else:
+        print("Optimization was not successful")
+
+print("Expected profit (Optimal objective):", optimal_objective)
+
+# Where do we earn revenue?
+revenue_DA =  sum( sum(p_DA_sol * lambda_DA[w,:] * pi[w] for w in WW) )
+revenue_RES = sum( sum(p_RES_sol * lambda_RES[w,:] * pi[w] for w in WW) )
+losses_ACT =  sum( sum(-a_RES_sol[w,:]*gamma_RES[w,:] * lambda_B[w,:] * pi[w] for w in WW) )
+revenue_BAL = sum( sum(-Delta_down_sol[w,:] * lambda_B[w,:] * pi[w] for w in WW) )
+
+print('These are the expected revenue streams:')
+print(f'Day-ahead market: {revenue_DA:>42.2f} DKK')
+print(f'aFRR capacity market (down): {revenue_RES:>31.2f} DKK')
+print(f'Money spent to buy el. back: {losses_ACT:>31.2f} DKK')
+print(f'Revenue from balancing market: {revenue_BAL:>29.2f} DKK')
+print(f'Summing these together yields the expected profit: {revenue_DA+revenue_RES+losses_ACT+revenue_BAL:.2f}={optimal_objective:.2f}')
+
+# Visualizations
+import matplotlib.pyplot as plt
+
+if show_plots:
+        fig, ax=plt.subplots(figsize=(6,4),dpi=500)
+        # p_RT.shape i 30 by 24 but for some reason it p_RT[t,:] is correct below and not p_RT[:,t]
+        for t in range(T): ax.plot(p_RT[t,:], label='$p^{RT}_{\omega,t}$' if t == 0 else None, alpha=0.5, color='tab:red')
+        ax.plot(p_DA_sol, label='$p^{DA}_t$', alpha=.8, color='tab:green')
+        ax.plot(p_RES_sol, label='$p^{RES}_t$', alpha=.8, color='tab:purple')
+        ax.legend(loc=0)
+        ax.set_xlabel('Hour of the day [h]')
+        ax.set_ylabel('Power [MW]')
+        plt.title('Examining the offer decisions: p_RES and p_RT')
+        plt.show()
+
+        fig, ax=plt.subplots(figsize=(6,4),dpi=500)
+        for t in range(T): ax.plot(a_RES_sol[t,:], label='$a^{RES}_{\omega,t}$' if t == 0 else None, alpha=0.5, color='tab:red')
+        ax.plot(p_DA_sol, label='$p^{DA}_t$', alpha=.8, color='tab:green')
+        ax.plot(p_RES_sol, label='$p^{RES}_t$', alpha=.8, color='tab:purple')
+        ax.legend(loc=0)
+        ax.set_xlabel('Hour of the day [h]')
+        ax.set_ylabel('Power [MW]')
+        plt.savefig('plots/V2/Step4_V2_decisions', dpi=500, bbox_inches='tight')
+        plt.title('Examining the offer decisions: p_RES and a_RES')
+        plt.show()
+
+        fig, ax=plt.subplots(figsize=(6,4),dpi=500)
+        ax.plot(p_DA_sol, label='$p^{DA}_t$', alpha=.8, color='tab:green')
+        ax.plot(p_RES_sol, label='$p^{RES}_t$', alpha=.8, color='tab:purple')
+        ax.plot([np.mean([p_RT[:,t]]) for t in range(T)], label='$\overline{p}^{RT}_t$', color='tab:red')
+
+        ax2 = ax.twinx()
+        ax2.plot([np.mean([lambda_DA[:,t]]) for t in range(T)] / np.array( [np.mean([lambda_B[:,t]]) for t in range(T)] ), label='$\overline{\lambda}^{DA}_t$ / $\overline{\lambda}^{B}_t$')
+        ax2.plot([np.mean([lambda_RES[:,t]]) for t in range(T)] / np.array( [np.mean([lambda_DA[:,t]]) for t in range(T)] ), label='$\overline{\lambda}^{RES}_t$ / $\overline{\lambda}^{DA}_t$')
+
+        lines, labels = ax.get_legend_handles_labels()
+        lines2,labels2 = ax2.get_legend_handles_labels()
+        ax.legend(lines+lines2,labels+labels2,loc=5)
+
+        ax.set_xlabel('Hour of the day [h]')
+        ax.set_ylabel('Power [MW]')
+        ax2.set_ylabel('Price ratio [-]')
+
+        plt.title('Offers in DA and RES and the ratio between DA- and BAL-prices')
+        plt.show()
+
+        fig, ax=plt.subplots(figsize=(6,4),dpi=500)
+        ax.plot(p_DA_sol, label='$p^{DA}_t$', alpha=.8, color='tab:green')
+        ax.plot(p_RES_sol, label='$p^{RES}_t$', alpha=.8, color='tab:purple')
+        ax.plot([np.mean([p_RT[:,t]]) for t in range(T)], label='$\overline{p}^{RT}_t$', color='tab:red')
+
+        ax2 = ax.twinx()
+        ax2.plot([np.mean(lambda_DA[:,t]) + np.mean(lambda_RES[:,t]) - np.mean(lambda_B[:,t]) for t in range(T)], label='E[P] 1MW DA')
+        ax2.axhline(y=0, alpha=.5, color='black')
+
+        lines, labels = ax.get_legend_handles_labels()
+        lines2,labels2 = ax2.get_legend_handles_labels()
+        ax.legend(lines+lines2,labels+labels2,loc=5)
+
+        ax.set_xlabel('Hour of the day [h]')
+        ax.set_ylabel('Power [MW]')
+        ax2.set_ylabel('Revenue - Expected profit of 1 MW DA offer [DKK]')
+        plt.savefig('plots/V2/Step4_V2_decisions_expP1MWDA', dpi=500, bbox_inches='tight')
+        plt.title('Offers in DA and RES and the ratio between DA- and BAL-prices')
+        plt.show()
+
+
+# Understanding the spread within our scenarios
+fig, ax = plt.subplots(2,2,figsize=(8,6))
+ax = ax.flatten()
+dict_params = {'$\lambda^{DA}$': lambda_DA, '$\lambda^{RES}$': lambda_RES, '$\lambda^{B}$':lambda_B, '$p^{RT}$':p_RT}
+for i,k in enumerate(dict_params.keys()):
+     ax[i].boxplot(dict_params[k])
+     ax[i].set_title(k)
+     ax[i].tick_params('x',rotation=45)
+plt.tight_layout()
+plt.show()
+
+print('##############\nScript is done\n##############')
